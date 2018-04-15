71225e2f7d9f38b5a489b2214a4668f43260a5e1
Matt Grf <g.matthieu49@gmail.com>
2018-04-15 17:29:34 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,25 +8,24 @@ device_path=device/samsung
 manifest=https://github.com/$device/manifest
 prompt="Pick an option :"
 
-title=Want you to sync
+title="Want you to sync"
 options=(yes no)
 echo $title
 PS3=$prompt
 
-select rom in ${options[@]};
+select option in ${options[@]};
     do
     case $REPLY in
     1 ) echo "syncing"
-	rm -rf .repo/local_manifests
-	git clone $manifest
-	repo sync --force-sync
-	rm -rf $device_path/$device/sepolicy $device_path/$common/sepolicy/*
-	git clone https://github.com/$device/sepolicy $device_path/$device/sepolicy
-	rm -rf $device_path/$common/ramdisk
-	git clone https://github.com/$device/ramdisk $device_path/$common/ramdisk
-	echo "You can now build for your $device" ;break;;
-	2 ) echo Goodbye!;sleep 2 && exit;;
+        rm -rf .repo/local_manifests
+        git clone $manifest .repo/local_manifests
+        repo sync --force-sync
+        rm -rf $device_path/$device/sepolicy $device_path/$common/sepolicy/*
+        git clone https://github.com/$device/sepolicy $device_path/$device/sepolicy
+        rm -rf $device_path/$common/ramdisk
+        git clone https://github.com/$device/ramdisk $device_path/$common/ramdisk
+        echo "You can now build for your $device" ;break;;
+        2 ) echo Goodbye!;sleep 2 && exit;;
     *) echo $check;continue;;
     esac
 done
-
